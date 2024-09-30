d36cb2b039cbd9c11cf15b714bd14fe082035ae7
Taluok <tanialuduenaok@gmail.com>
2024-09-30 13:01:44 -0300

    fixed errors

--- fastBite-client/src/pages/shop/CartPage.jsx
+++ fastBite-client/src/pages/shop/CartPage.jsx
@@ -1,68 +1,98 @@
-import React, { useContext, useState } from 'react';
-import useCart from '../../hooks/useCart';
+import React, { useContext, useEffect, useState } from "react";
+import useCart from "../../hooks/useCart";
+import { AuthContext } from "../../contexts/AuthProvider";
+import Swal from "sweetalert2";
 import { FaTrash } from "react-icons/fa";
-import Swal from 'sweetalert2';
-import { AuthContext } from '../../contexts/AuthProvider';
+import { Link } from 'react-router-dom'
+import axios from "axios";
 
 const CartPage = () => {
-    const [cart, refetch] = useCart();
     const { user } = useContext(AuthContext);
+    const [cart, refetch] = useCart();
+    console.log(cart)
+    const [cartItems, setCartItems] = useState([]);
+    // console.log(cartItems)
 
-    // Calcular precio total por item
-    const calculatePrice = (item) => {
+    // Calculate the total price for each item in the cart
+    const calculateTotalPrice = (item) => {
         return item.price * item.quantity;
     };
-
-    // Función para disminuir la cantidad
-    const handleDecrease = (item) => {
-        if (item.quantity > 1) {  // No permitir cantidades menores a 1
-            fetch(`http://localhost:6001/carts/${item._id}`, {
+    // Handle quantity increase
+    const handleIncrease = async (item) => {
+        try {
+            const response = await fetch(`http://localhost:6001/carts/${item._id}`, {
                 method: "PUT",
                 headers: {
-                    'Content-Type': 'application/json'
+                    "Content-Type": "application/json",
                 },
-                body: JSON.stringify({ quantity: item.quantity - 1 })
-            })
-                .then(res => res.json())
-                .then(data => {
-                    if (data.success) {
-                        refetch(); // Actualizar carrito tras la petición
-                    } else {
-                        console.error('Error updating quantity:', data.message);
+                body: JSON.stringify({ quantity: item.quantity + 1 }),
+            });
+
+            if (response.ok) {
+                const updatedCart = cartItems.map((cartItem) => {
+                    if (cartItem.id === item.id) {
+                        return {
+                            ...cartItem,
+                            quantity: cartItem.quantity + 1,
+                        };
                     }
-                })
-                .catch(error => console.error('Error:', error));
+                    return cartItem;
+                });
+                await refetch();
+                setCartItems(updatedCart);
+            } else {
+                console.error("Failed to update quantity");
+            }
+        } catch (error) {
+            console.error("Error updating quantity:", error);
+        }
+    };
+    // Handle quantity decrease
+    const handleDecrease = async (item) => {
+        if (item.quantity > 1) {
+            try {
+                const response = await fetch(
+                    `http://localhost:6001/carts/${item._id}`,
+                    {
+                        method: "PUT",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({ quantity: item.quantity - 1 }),
+                    }
+                );
+
+                if (response.ok) {
+                    const updatedCart = cartItems.map((cartItem) => {
+                        if (cartItem.id === item.id) {
+                            return {
+                                ...cartItem,
+                                quantity: cartItem.quantity - 1,
+                            };
+                        }
+                        return cartItem;
+                    });
+                    await refetch();
+                    setCartItems(updatedCart);
+                } else {
+                    console.error("Failed to update quantity");
+                }
+            } catch (error) {
+                console.error("Error updating quantity:", error);
+            }
         }
     };
 
-    //calculo precio total 
-    const cartSubTotal = cart.reduce((total, item) => {
-        return total + calculatePrice(item);
-    },0);
+    // Calculate the cart subtotal
+    const cartSubtotal = cart.reduce((total, item) => {
+        return total + calculateTotalPrice(item);
+    }, 0);
 
-    const orderTotal = cartSubTotal;
+    // Calculate the order total
+    const orderTotal = cartSubtotal;
+    // console.log(orderTotal)
 
-    // Función para aumentar la cantidad
-    const handleIncrease = (item) => {
-        fetch(`http://localhost:6001/carts/${item._id}`, {
-            method: "PUT",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ quantity: item.quantity + 1 })
-        })
-            .then(res => res.json())
-            .then(data => {
-                if (data.success) {
-                    refetch();
-                } else {
-                    console.error('Error updating quantity:', data.message);
-                }
-            })
-            .catch(error => console.error('Error:', error));
-    };
-
-    // Manejar la eliminación del item
+    // delete an item
     const handleDelete = (item) => {
         Swal.fire({
             title: "Are you sure?",
@@ -71,122 +101,133 @@ const CartPage = () => {
             showCancelButton: true,
             confirmButtonColor: "#3085d6",
             cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
+            confirmButtonText: "Yes, delete it!",
         }).then((result) => {
             if (result.isConfirmed) {
-                fetch(`http://localhost:6001/carts/${item._id}`, {
-                    method: "DELETE"
+                axios.delete(`http://localhost:6001/carts/${item._id}`).then(response => {
+                    if (response) {
+                        refetch();
+                        Swal.fire("Deleted!", "Your file has been deleted.", "success");
+                    }
                 })
-                    .then(res => res.json())
-                    .then(data => {
-                        if (data.deletedCount > 0) {
-                            refetch();
-                            Swal.fire({
-                                title: "Deleted!",
-                                text: "Your item has been deleted.",
-                                icon: "success"
-                            });
-                        }
-                    })
                     .catch(error => {
-                        console.error("Error deleting item:", error);
-                        Swal.fire({
-                            title: "Error!",
-                            text: "There was an issue deleting the item.",
-                            icon: "error"
-                        });
+                        console.error(error);
                     });
             }
         });
     };
 
     return (
-        <div className="section-container">
-            {/* Banner */}
-            <div className='py-36 flex flex-col items-center justify-center gap-8'>
-                {/* Textos */}
-                <div className='px-4 space-y-7 text-center'>
-                    <h2 className='md:text-5xl text-4xl font-bold md:leading-snug leading-snug'>
-                        Items Added to the <span className='text-orange'>Cart</span>
-                    </h2>
-                    <p className='text-[#4a4a4a] text-xl'>
-                        Where Each Plate Weaves a Story of Culinary Mastery & Passionate Craftsmanship
-                    </p>
-                </div>
-
-                {/* Tabla */}
-                <div className="overflow-x-auto">
-                    <table className="table">
-                        {/* Cabecera */}
-                        <thead className='bg-orange text-white rounded-sm'>
-                            <tr>
-                                <th>#</th>
-                                <th>Food</th>
-                                <th>Item Name</th>
-                                <th>Quantity</th>
-                                <th>Price</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {/* Filas dinámicas del carrito */}
-                            {cart.map((item, index) => (
-                                <tr key={item._id}>
-                                    <td>{index + 1}</td>
-                                    <td>
-                                        <div className="flex items-center gap-3">
-                                            <div className="avatar">
-                                                <div className="mask mask-squircle h-12 w-12">
-                                                    <img src={item.image} alt={item.name} />
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td className='font-medium'>{item.name}</td>
-                                    <td>
-                                        <button className='btn btn-xs' onClick={() => handleDecrease(item)}>-</button>
-                                        <input type="number" value={item.quantity}
-                                            readOnly className='w-10 mx-2 text-center overflow-hidden appearance-none' />
-                                        <button className='btn btn-xs' onClick={() => handleIncrease(item)}>+</button>
-                                    </td>
-                                    <td>${calculatePrice(item)}</td>
-                                    <td>
-                                        <button
-                                            className="btn btn-ghost btn-xs text-red"
-                                            onClick={() => handleDelete(item)}
-                                        >
-                                            <FaTrash className="inline mr-1" /> Remove
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                        {/* Pie de tabla */}
-                        <tfoot>
-                            <tr>
-                                <th></th>
-                                <th>Food</th>
-                                <th>Item Name</th>
-                                <th>Quantity</th>
-                                <th>Price</th>
-                                <th></th>
-                            </tr>
-                        </tfoot>
-                    </table>
-                </div>
-
-                {/* Detalles del cliente */}
-                {user ? ( // Verifica si 'user' no es null
-                    <div className='md:w-1/2 space-y-3'>
-                        <h3 className='font-medium'>Customer Details</h3>
-                        <p>Name: {user.displayName}</p>
-                        <p>Email: {user.email}</p>
-                        <p>User ID: {user._id}</p>
+        <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
+            {/* banner */}
+            <div className=" bg-gradient-to-r from-0% from-[#FAFAFA] to-[#FCFCFC] to-100%">
+                <div className="py-28 flex flex-col items-center justify-center">
+                    {/* content */}
+                    <div className=" text-center px-4 space-y-7">
+                        <h2 className="md:text-5xl text-4xl font-bold md:leading-snug leading-snug">
+                            Items Added to The<span className="text-green"> Cart</span>
+                        </h2>
                     </div>
-                ) : (
-                    <p>Please log in to see your customer details.</p>
-                )}
+                </div>
             </div>
+
+            {/* cart table */}
+
+            {
+                (cart.length > 0) ? <div>
+                    <div className="">
+                        <div className="overflow-x-auto">
+                            <table className="table">
+                                {/* head */}
+                                <thead className="bg-green text-white rounded-sm">
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Food</th>
+                                        <th>Item Name</th>
+                                        <th>Quantity</th>
+                                        <th>Price</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {cart.map((item, index) => (
+                                        <tr key={index}>
+                                            <td>{index + 1}</td>
+                                            <td>
+                                                <div className="avatar">
+                                                    <div className="mask mask-squircle w-12 h-12">
+                                                        <img
+                                                            src={item.image}
+                                                            alt="Avatar Tailwind CSS Component"
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td className="font-medium">{item.name}</td>
+                                            <td>
+                                                <button
+                                                    className="btn btn-xs"
+                                                    onClick={() => handleDecrease(item)}
+                                                >
+                                                    -
+                                                </button>
+                                                <input
+                                                    type="number"
+                                                    value={item.quantity}
+                                                    onChange={() => console.log(item.quantity)}
+                                                    className="w-10 mx-2 text-center overflow-hidden appearance-none"
+                                                />
+                                                <button
+                                                    className="btn btn-xs"
+                                                    onClick={() => handleIncrease(item)}
+                                                >
+                                                    +
+                                                </button>
+                                            </td>
+                                            <td>${calculateTotalPrice(item).toFixed(2)}</td>
+                                            <td>
+                                                <button
+                                                    className="btn btn-sm border-none text-red bg-transparent"
+                                                    onClick={() => handleDelete(item)}
+                                                >
+                                                    <FaTrash />
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                                {/* foot */}
+                            </table>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="flex flex-col md:flex-row justify-between items-start my-12 gap-8">
+                        <div className="md:w-1/2 space-y-3">
+                            <h3 className="text-lg font-semibold">Customer Details</h3>
+                            <p>Name: {user?.displayName || "None"}</p>
+                            <p>Email: {user?.email}</p>
+                            <p>
+                                User_id: <span className="text-sm">{user?.uid}</span>
+                            </p>
+                        </div>
+                        <div className="md:w-1/2 space-y-3">
+                            <h3 className="text-lg font-semibold">Shopping Details</h3>
+                            <p>Total Items: {cart.length}</p>
+                            <p>
+                                Total Price:{" "}
+                                <span id="total-price">${orderTotal.toFixed(2)}</span>
+                            </p>
+                            <button className="btn btn-md bg-green text-white px-8 py-1">
+                                Procceed to Checkout
+                            </button>
+                        </div>
+                    </div>
+                </div> : <div className="text-center mt-20">
+                    <p>Cart is empty. Please add products.</p>
+                    <Link to="/menu"><button className="btn bg-green text-white mt-3">Back to Menu</button></Link>
+                </div>
+            }
+
         </div>
     );
 };
--- fastBite-server/index.js
+++ fastBite-server/index.js
@@ -14,12 +14,33 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@demo-fastbite-cluster.1mpra.mongodb.net/?retryWrites=true&w=majority&appName=demo-fastBite-cluster`;
 
+// jwt authentication
+app.post('/jwt', async (req, res) => {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1hr'
+    })
+    res.send({ token });
+})
+
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
     }
+});
+
+//   import routes here
+const menuRoutes = require('./api/routes/menuRoutes');
+const cartRoutes = require('./api/routes/cartRoutes');
+const userRoutes = require('./api/routes/userRoutes')
+app.use('/menu', menuRoutes)
+app.use('/carts', cartRoutes);
+app.use('/users', userRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Hello Foodi Client Server!");
 });
 
 async function run() {
@@ -32,47 +53,47 @@ async function run() {
         const cartCollections = client.db("demo-fastBite-client").collection("cartItems")
 
         //all menu items operations
-        app.get('/menu', async(req, res) => {
+        app.get('/menu', async (req, res) => {
             const result = await menuCollections.find().toArray()
             res.send(result)
         })
 
         //all carts operations 
-        app.post('/carts', async(req, res) => {
+        app.post('/carts', async (req, res) => {
             const cartItems = req.body;
             const result = await cartCollections.insertOne(cartItems)
             res.send(result)
         })
 
         //get carts using email
-        app.get('carts', async(req, res) => {
+        app.get('carts', async (req, res) => {
             const email = req.query.email;
-            const filter = {email: email};
+            const filter = { email: email };
             const result = await cartCollections.find(filter).toArray();
             res.send(result)
         })
 
         //get specific cats 
-        app.get('/carts/:id', async(req, res) => {
+        app.get('/carts/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)};
+            const filter = { _id: new ObjectId(id) };
             const result = await cartCollections.findOne(filter);
             res.send(result)
         })
 
         //delete items from cart
-        app.delete('/carts/:id', async(req,res) => {
-            const id =  req.params.id;
-            const filter = {_id: new ObjectId(id)};
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
             const result = await cartCollections.deleteOne(filter);
             res.send(result)
         })
 
         //update cats quantity
-        app.put("/cats/:id", async(req, res) => {
+        app.put("/cats/:id", async (req, res) => {
             const id = req.params.id;
-            const {quantity} = req.body
-            const filter = {_id: new ObjectId(id)};
+            const { quantity } = req.body
+            const filter = { _id: new ObjectId(id) };
             const options = { upsert: true };
 
             const updateDoc = {
@@ -89,7 +110,7 @@ async function run() {
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
     } finally {
-        
+
     }
 }
 run().catch(console.dir);
