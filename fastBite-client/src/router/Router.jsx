c3a7749635e99ebfc3c3c3c9cc96de59cddef995
Taluok <tanialuduenaok@gmail.com>
2024-10-01 10:33:59 -0300

    Handle unauthorized access

--- fastBite-client/src/router/Router.jsx
+++ fastBite-client/src/router/Router.jsx
@@ -3,8 +3,17 @@ import Main from "../layout/Main";
 import Home from "../pages/home/Home";
 import Menu from "../pages/shop/Menu";
 import Signup from "../components/Sigup";
+import Order from "../pages/dashboard/Order";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import UserProfile from "../pages/dashboard/UserProfile";
 import CartPage from "../pages/shop/CartPage";
-import UpdateProfile from "../pages/dashboard/UpdateProfile";
+import Login from "../components/Login";
+import DashboardLayout from "../layout/DashboardLayout";
+import Dashboard from "../pages/dashboard/admin/Dashboard";
+import Users from "../pages/dashboard/admin/Users";
+import AddMenu from "../pages/dashboard/admin/AddMenu";
+import ManageItems from "../pages/dashboard/admin/ManageItems";
+import UpdateMenu from "../pages/dashboard/admin/UpdateMenu";
 
 const router = createBrowserRouter([
     {
@@ -13,25 +22,60 @@ const router = createBrowserRouter([
         children: [
             {
                 path: "/",
-                element: <Home />,
+                element: <Home />
             },
             {
                 path: "/menu",
-                element: <Menu />,
+                element: <Menu />
+            },
+            {
+                path: "/order",
+                element: <PrivateRoute><Order /></PrivateRoute>
+            },
+            {
+                path: "/update-profile",
+                element: <UserProfile />
             },
             {
                 path: "/cart-page",
                 element: <CartPage />
-            },
-            {
-                path: "/update-profile",
-                element: <UpdateProfile />
             }
-        ],
+        ]
     },
     {
         path: "/signup",
         element: <Signup />
+    },
+    {
+        path: "/login",
+        element: <Login />
+    },
+    {
+        path: 'dashboard',
+        element: <PrivateRoute><DashboardLayout /></PrivateRoute>,
+        children: [
+            {
+                path: '',
+                element: <Dashboard />
+            },
+            {
+                path: 'users',
+                element: <Users />
+            },
+            {
+                path: 'add-menu',
+                element: <AddMenu />
+            },
+            {
+                path: "manage-items",
+                element: <ManageItems />
+            },
+            {
+                path: "update-menu/:id",
+                element: <UpdateMenu />,
+                loader: ({ params }) => fetch(`http://localhost:6001/menu/${params.id}`)
+            }
+        ]
     }
 ]);
 
